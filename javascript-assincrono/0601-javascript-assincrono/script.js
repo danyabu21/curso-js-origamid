d4ff3c5bcffbb618e4e14e699221b2c04ca08f34
danielyabu21 <danielyabu21@gmail.com>
2022-09-18 10:06:29 -0300

    JavaScript Assíncrono

---
+++ javascript-assincrono/0601-javascript-assincrono/script.js
@@ -0,0 +1,26 @@
+// setTimeout(function () {
+//   console.log("Timer 01");
+// });
+
+// setTimeout(function () {
+//   console.log("Timer 03");
+// }, 500);
+
+// console.log("Log 1");
+
+// setTimeout(function () {
+//   console.log("Timer 02");
+// }, 1000);
+
+// console.log("Log 2");
+
+// Vantagens de usar um JavaScript Assíncrono:
+
+/* 
+-Carregamento de fundo: Não trava o script. É possível utilizar o site enquanto o processamento é realizado de fundo.
+
+-Função ao término: Podemos ficar de olho no carregamento e executar uma função assim que ela terminar.
+
+-Requisição ao servidor: Não precisamos recarregar a página por completo à cada requisição feita ao servidor.
+
+*/
